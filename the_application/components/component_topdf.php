eda7a7b37195a8108843c8135c8c6e7494d9378f
eacevedof <eacevedof@yahoo.es>
2017-10-08 14:30:34 +0200

    component_topdf.php 1.0.0

--- the_application/components/component_topdf.php
+++ the_application/components/component_topdf.php
@@ -3,8 +3,7 @@
  * @author Eduardo Acevedo Farje.
  * @link www.eduardoaf.com
  * @name ComponentTopdf
- * @file component_topdf.php
- * @version 1.0.0
+ * @file component_topdf.php 1.0.0
  * @date 01-06-2014 12:45
  * @observations
  */
