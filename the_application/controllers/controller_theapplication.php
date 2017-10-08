1faa3bf0c162632104e5fa23459b78f1f1de8c50
eacevedof <eacevedof@yahoo.es>
2017-10-08 11:49:10 +0200

    TheApplication\Controllers\TheApplicationController comentarios

--- the_application/controllers/controller_theapplication.php
+++ the_application/controllers/controller_theapplication.php
@@ -2,7 +2,7 @@
 /**
  * @author Eduardo Acevedo Farje.
  * @link www.eduardoaf.com
- * @name TheApplication\Controllers\TheApplication
+ * @name TheApplication\Controllers\TheApplicationController
  * @file controller_theapplication.php 
  * @version 1.0.0
  * @date 08-10-2017 08:44 (SPAIN)
@@ -20,4 +20,4 @@ class TheApplicationController extends TheFrameworkController
 
     }
 
-}//TheApplication
+}//TheApplicationController
