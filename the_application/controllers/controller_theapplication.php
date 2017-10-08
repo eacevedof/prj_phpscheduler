888e9c9fbde24868bc3431b0e6116e0ee8fb747f
eacevedof <eacevedof@yahoo.es>
2017-10-08 08:55:01 +0200

    Hasta aqui nada afectado todo ok

---
+++ the_application/controllers/controller_theapplication.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link www.eduardoaf.com
+ * @name TheApplication\Controllers\TheApplication
+ * @file controller_theapplication.php 
+ * @version 1.0.0
+ * @date 08-10-2017 08:44 (SPAIN)
+ * @observations:
+ * @requires  
+ */
+namespace TheApplication\Controllers;
+
+class TheApplication
+{
+    public function __construct()
+    {
+
+    }
+
+}//TheApplication
