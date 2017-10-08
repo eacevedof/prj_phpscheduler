33766c1ca6356382526d845e84fe9fe6b41b17b4
eacevedof <eacevedof@yahoo.es>
2017-10-08 18:53:38 +0200

    Footer

--- the_reactjs/src/App.js
+++ the_reactjs/src/App.js
@@ -29,6 +29,27 @@ class App extends Component {
                         </Col>
                     </Row>
                 </Grid>
+                <footer class="footer">
+                    <div class="container">
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                <a rel="nofollow"  class="btn btn-block" href="/"> 
+                                    <span class="fa fa-home"></span>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a rel="nofollow" class="btn btn-block btn-social btn-github" href="https://github.com/eacevedof/prj_phpscheduler"> 
+                                    <span class="fa fa-github"></span>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a rel="nofollow"  href="https://twitter.com/eacevedof" class="btn btn-block btn-social btn-twitter"> 
+                                    <span class="fa fa-twitter"></span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </footer>                
             </div>
         ) //return
     }//render
