fb9c4abe0d9fb247acf09856f4bed25253b6dee5
eacevedof <eacevedof@yahoo.es>
2017-10-08 22:07:56 +0200

    vuelve a funcionar todo sin el router

--- the_reactjs/src/App.js
+++ the_reactjs/src/App.js
@@ -1,39 +1,16 @@
 import React, { Component } from "react";
-import { Navbar, Grid, Row, Col } from "react-bootstrap";
-import ProductList from "./components/ProductList";
-import ShoppingCart from "./components/ShoppingCart";
-import ElementFooter from "./components/elements/ElementFooter";
+import { Router, Route, browserHistory } from "react-router" 
+import Home from "./components/Home";
 import "./App.css";
-
-console.log("APP")
 
 class App extends Component {
     render() {
-        console.log("APP.App.render")
         return (
-            <div className="container-fluid">
-                <Navbar inverse staticTop>
-                    <Navbar.Header>
-                        <Navbar.Brand>
-                            <a href="#">Ecommerce</a>
-                        </Navbar.Brand>
-                    </Navbar.Header>
-                </Navbar>
-
-                <Grid>
-                    <Row>
-                        <Col sm={8}>
-                            <ProductList />
-                        </Col>
-                        <Col sm={4}>
-                            <ShoppingCart />
-                        </Col>
-                    </Row>
-                </Grid>
-                <ElementFooter/>
-            </div>
-        ) //return
+            <Home/>
+        )//return
     }//render
 }//App
 
 export default App;
+    
+  
