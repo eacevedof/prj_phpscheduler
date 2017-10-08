bfcca4c679735b74f1b91433f2f6cd3be0895aa4
eacevedof <eacevedof@yahoo.es>
2017-10-08 08:51:12 +0200

    <prj>/the_vendor/paths.php 1.0.1

--- the_vendor/paths.php
+++ the_vendor/paths.php
@@ -1,4 +1,8 @@
 <?php
+//<prj>/the_vendor/paths.php 1.0.1
 //carpetas instaladas sin composer
 //estas ruta se cargan al arrancar el index
-$arVendors = ["fpdf"];
+$arVendors = [
+    "fpdf"
+    ,"theframework/controllers"
+];
