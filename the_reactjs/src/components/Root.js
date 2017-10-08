561ad3e1aa5de909c8f8aa5f83ee77b716c05b34
eacevedof <eacevedof@yahoo.es>
2017-10-08 22:47:48 +0200

    Ya funciona el router. Se necesitaba react-router-dom

--- the_reactjs/src/components/Root.js
+++ the_reactjs/src/components/Root.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Provider } from 'react-redux'
-import { Router, Route } from 'react-router'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import App from '../App'
 
 const Root = ({ store }) => (
     <Provider store={store}>
-        <App/>
+        <Router>
+            <Route exact path='/' component={App} />
+        </Router>
     </Provider>
 )
 
