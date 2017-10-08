911dd59ed56bade666ad21990e21765fa61dee2e
eacevedof <eacevedof@yahoo.es>
2017-10-08 11:48:31 +0200

    Configuro TheApplicationModel

---
+++ the_application/models/homes/model_schedule.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link www.eduardoaf.com
+ * @name TheApplication\Models\ModelSchedule
+ * @file model_schedule.php 
+ * @version 1.0.0
+ * @date 08-10-2017 08:44 (SPAIN)
+ * @observations:
+ * @requires  
+ */
+namespace TheApplication\Models;
+
+use TheApplication\Models\TheApplicationModel;
+
+class ModelSchedule extends TheApplicationModel
+{
+    public function __construct()
+    {
+
+    }
+
+}//ModelSchedule
---
+++ the_application/models/model_theapplication.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link www.eduardoaf.com
+ * @name TheApplication\Models\TheApplicationModel
+ * @file model_theapplication.php 
+ * @version 1.0.0
+ * @date 08-10-2017 08:44 (SPAIN)
+ * @observations:
+ * @requires  
+ */
+namespace TheApplication\Models;
+
+use TheFramework\Main\TheFrameworkModel;
+
+class TheApplicationModel extends TheFrameworkModel
+{
+    public function __construct()
+    {
+
+    }
+
+}//TheApplicationModel
