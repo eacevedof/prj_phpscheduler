0c1503bfc1f7d095fc1a9e34f4790ac272bfe703
eacevedof <eacevedof@yahoo.es>
2017-10-11 09:40:48 +0200

    temp_a: file_app.js

--- the_reactjs/src/temp/temp_a.js
+++ the_reactjs/src/temp/temp_a.js
@@ -758,3 +758,44 @@ const fnMapDispatchToProps = fnDispatch => {
 }
 
 export default connect(fnMapStateToProps,fnMapDispatchToProps)(fnProductList);
+//=================================================
+//               APP.JS
+//=================================================  
+//C:\xampp\htdocs\prj_reactjs\src\App.js
+import React, { Component } from 'react';
+import { Navbar, Grid, Row, Col } from 'react-bootstrap';
+import ProductList from './components/ProductList';
+import ShoppingCart from './components/ShoppingCart';
+import './App.css';
+
+console.log("load 5: App.js")
+
+class App extends Component {
+    render() {
+        console.log("APP.App.render")
+        return (
+            <div>
+                <Navbar inverse staticTop>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="#">Ecommerce</a>
+                        </Navbar.Brand>
+                    </Navbar.Header>
+                </Navbar>
+
+                <Grid>
+                    <Row>
+                        <Col sm={8}>
+                            <ProductList />
+                        </Col>
+                        <Col sm={4}>
+                            <ShoppingCart />
+                        </Col>
+                    </Row>
+                </Grid>
+            </div>
+        ) //return
+    }//render
+}//App
+
+export default App;
