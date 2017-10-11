37c6f635d841337676b522dd2708bd3b3ea8e6a0
eacevedof <eacevedof@yahoo.es>
2017-10-11 09:20:43 +0200

    temp_a: applyMiddleware

--- the_reactjs/src/temp/temp_a.js
+++ the_reactjs/src/temp/temp_a.js
@@ -1,7 +1,58 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+//=================================================
+//                REDUX-THUNK.JS
+//=================================================
+//C:\xampp\htdocs\prj_reactjs\node_modules\redux-thunk\src\index.js
+function createThunkMiddleware(extraArgument) {
+  return ({ dispatch, getState }) => next => action => {
+    if (typeof action === 'function') {
+      return action(dispatch, getState, extraArgument);
+    }
+
+    return next(action);
+  };
+}
+
+const thunk = createThunkMiddleware();
+thunk.withExtraArgument = createThunkMiddleware;
+
+export default thunk;
+//=================================================
+//               REDUX.JS (MIDDLEWARE)
+//=================================================
+//C:\xampp\htdocs\prj_reactjs\node_modules\redux\src\applyMiddleware.js
+import compose from './compose'
+/**
+ * Creates a store enhancer that applies middleware to the dispatch method
+ * of the Redux store. This is handy for a variety of tasks, such as expressing
+ * asynchronous actions in a concise manner, or logging every action payload.
+ *
+ * See `redux-thunk` package as an example of the Redux middleware.
+ *
+ * Because middleware is potentially asynchronous, this should be the first
+ * store enhancer in the composition chain.
+ *
+ * Note that each middleware will be given the `dispatch` and `getState` functions
+ * as named arguments.
+ *
+ * @param {...Function} middlewares The middleware chain to be applied.
+ * @returns {Function} A store enhancer applying the middleware.
  */
+export default function applyMiddleware(...middlewares) {
+  return (createStore) => (reducer, preloadedState, enhancer) => {
+    const store = createStore(reducer, preloadedState, enhancer)
+    let dispatch = store.dispatch
+    let chain = []
 
+    const middlewareAPI = {
+      getState: store.getState,
+      dispatch: (action) => dispatch(action)
+    }
+    chain = middlewares.map(middleware => middleware(middlewareAPI))
+    dispatch = compose(...chain)(store.dispatch)
 
+    return {
+      ...store,
+      dispatch
+    }
+  }
+}
