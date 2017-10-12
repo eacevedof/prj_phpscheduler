869d7a0daa36d9626c3949cd1b978147e9534950
eduardoaf.com <eacevedof@yahoo.es>
2017-10-12 19:20:20 +0200

    comento arbol de llamada

--- the_reactjs/src/temp/temp_b.js
+++ the_reactjs/src/temp/temp_b.js
@@ -1,7 +1,30 @@
-Button.onClick() => 
-    oDispatch.fnAddtoCart(oProduct) =>
-        fnAcAddtoCart(oProduct) => 
-            fnDispatch(oAction) =>
-                fnLogger(oStore,fnNext,oAction) =>
-                    fnStoreCart(arState,oAction) =>
-                        arState
+Button.onClick() : => 
+    oDispatch.fnAddtoCart(oProduct) : => //PRODUCTLIST.fnMapDispatchToProps.oDispatch.fnAddToCart
+        fnAcAddtoCart(oProduct) : => //ACTIONCREATORS.fnAcAddToCart 
+            fnDispatch(oAction) : => //PRODUCTLIST.fnLogger(oAction)
+                fnLogger(oStore,fnNext,oAction) => //STORE.fnLogger
+                    //REDUCERS combinados
+                    fnStoreCart(arState,oAction) => //STORE.fnStoreCart
+                    fnStoreProducts(arState,oAction) => //STORE.fnStoreProducts
+                        
+
+
+PRODUCTLIST.fnMapDispatchToProps.oDispatch.fnAddToCart
+actionCreators.js:11 ACTIONCREATORS.fnAcAddToCart return oAction+oProduct
+store.js:47 STORE.fnLogger dispatching oAction:  {type: "ADD_TO_CART", product: {…}}
+store.js:48 STORE.fnLogger fnNext:  ƒ (action) {
+if (typeof action === 'function') {
+return action(dispatch, getState, extraArgument);
+}
+
+return next(action);
+}
+store.js:31 STORE.fnStoreCart.oAction.type ADD_TO_CART
+store.js:32 STORE.fnStoreCart.arState []
+store.js:19 STORE.fnStoreProducts.oAction.type ADD_TO_CART
+store.js:20 STORE.fnStoreProducts.arState (3) [{…}, {…}, {…}]
+ProductList.js:49 PRODUCTLIST.fnMapStateToProps return oStateNew con arProducts
+ShoppingCart.js:57 SHOPPINGCART.fnMapStateToProps return oStateNew con arCart
+store.js:50 STORE.fnLogger oStore.getstate() next state:  {arCart: Array(1), arProducts: Array(3)}
+store.js:51 STORE.fnLogger oResult:  {type: "ADD_TO_CART", product: {…}}
+ShoppingCart.js:25 SHOPPINGCART.fnRenderShoppingCart
