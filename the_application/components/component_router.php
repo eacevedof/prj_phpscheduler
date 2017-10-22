5fb30ef3fbf04af0f25958f511a6c47ab353a45e
eacevedof <eacevedof@yahoo.es>
2017-10-22 17:24:39 +0200

    router y log

--- the_application/components/component_router.php
+++ the_application/components/component_router.php
@@ -15,6 +15,9 @@ class ComponentRouter
 {
 
     private static $sReqUri;
+    private static $iReqParts;
+    private static $arReqParts;
+    
     private static $arMessages = [];
     private static $arUrls = [];
     public static $arRun = [];
@@ -30,10 +33,9 @@ class ComponentRouter
         self::$arUrls[]=["url"=>$sUrl,"controller"=>$sController,"nscontroller"=>$sNSClass,"method"=>$sMethod];
     }
    
-    public static function run()
+    private static function check_exact()
     {
-        self::add();
-        self::$sReqUri = $_SERVER["REQUEST_URI"];
+        //bug(self::$arUrls,self::$sReqUri);die;
         foreach(self::$arUrls as $arUrl)
         {
             if($arUrl["url"]==self::$sReqUri)
@@ -42,6 +44,75 @@ class ComponentRouter
                 return self::$arRun;
             }
         }
+        return [];        
+    }
+    
+    private static function check_pattern()
+    {
+        $sReqUri = self::$sReqUri;
+        $arUrls = self::$arUrls;
+        $iReqParts = self::$iReqParts;
+        $arReqUri = self::$arReqParts;
+        
+        //bug($arReqUri,"arReqUri: $sReqUri");
+        
+        $arExploded = [];
+        foreach($arUrls as $i=>$arUrl)
+            $arExploded[$i] = explode("/",$arUrl["url"]);
+        
+        //bug($arExploded);
+        foreach($arExploded as $iUrl=>$arPattParts)
+        {
+            //tienen los mismos trozos
+            if(count($arPattParts)==$iReqParts)
+            {
+                //pr("count ok");
+                $isOk = TRUE;
+                $arGet = [];
+                foreach($arPattParts as $iPart=>$sPattPart)
+                {
+                    //pr($arReqUri[$iPart],"$iPart: $sPattPart");
+                    if(strstr($sPattPart,":"))
+                    {
+                        $arGet[$sPattPart] = $arReqUri[$iPart];
+                        $isOk = (TRUE && $isOk);
+                    }
+                    else
+                    {
+                        $isOk = ($isOk && ($arReqUri[$iPart]==$sPattPart));
+                    }
+                }//foreach($arUrlParts)
+                
+                //bug($isOk,"isok");
+                if($isOk)
+                {
+                    foreach($arGet as $sK=>$sValue)
+                    {
+                        $sK = str_replace(":","",$sK);
+                        $_GET[$sK] = $sValue;
+                    }
+                    return self::$arUrls[$iUrl];
+                }
+            }//if(count)
+        }//foreach($arExploded)
+        
+        return [];
+    }//check_pattern
+    
+    public static function run()
+    {
+        self::add();
+        self::$sReqUri = $_SERVER["REQUEST_URI"];
+        self::$arReqParts = explode("/",self::$sReqUri);
+        self::$iReqParts = count(self::$arReqParts);
+        $_GET["REQUEST_URI"]["uri"] = self::$sReqUri;
+        $_GET["REQUEST_URI"]["parts"] = self::$arReqParts;
+        
+        if(self::check_exact())
+            return self::check_exact();
+        else
+            return self::check_pattern();
+        
         return [];
     }
    
