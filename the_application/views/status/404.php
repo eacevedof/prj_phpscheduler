1270e1bccb0846c4b147fdbed7e84782646c692f
eacevedof <eacevedof@yahoo.es>
2017-10-22 17:15:52 +0200

    404

--- the_application/views/status/404.php
+++ the_application/views/status/404.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
+    <title>404 - Content not found!</title>
+    <meta name="description" content="404 - Content not found!">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 </head>
 <body>
